4fe58607c35b77cd749068aeac9fec08de063b43
elihinshaw <eghinshaw@gmail.com>
2023-09-05 17:50:43 -0500

    added bootstrap 5 header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
+    <AdventureHeader />
     <AdventureLocations />
     <AdventureMap />
   </div>
 </template>
 <script>
 import AdventureLocations from "./components/AdventureLocations.vue";
+import AdventureHeader from "./components/AdventureHeader.vue";
 import AdventureMap from "./components/AdventureMap.vue";
 import "./app.css";
 export default {
+  compatConfig: { MODE: 3 },
   name: "App",
   components: {
     AdventureLocations,
     AdventureMap,
+    AdventureHeader,
   },
 };
 </script>
