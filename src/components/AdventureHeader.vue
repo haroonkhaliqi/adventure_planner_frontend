e51d1c677d7c8deeab7d4218b95d118452af48cb
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-09-05 17:45:46 -0700

    added navbar and logo

--- src/components/AdventureHeader.vue
+++ src/components/AdventureHeader.vue
@@ -1,25 +1,50 @@
 <template>
   <header class="mb-4">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Download</a>
-              </li>
-            </ul>
-          </div>
+      <div class="container">
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="#">
+            <img :src="logo" alt="Logo" :width="60" :height="60" />
+            WanderWaze
+          </b-navbar-brand>
+        </b-navbar>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Download</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </header>
-  </template>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import logo from "@/assets/logo.png"; // Adjust the path as needed
+
+export default {
+  data() {
+    return {
+      logo: logo,
+    };
+  },
+};
+</script>
