2baecf038ed081749d0eb81cc3ec1234cb2b9681
elihinshaw <eghinshaw@gmail.com>
2023-09-08 14:25:44 -0500

    Added isAutenticated to sessionStorage and uses that to check instead, so upon page refresh the state value persists

--- src/storeauth.js
+++ src/storeauth.js
@@ -1,15 +1,18 @@
-import Vuex from "vuex"
+import Vuex from "vuex";
 
 const store = new Vuex.Store({
   state: {
-    isAuthenticated: false,
+    // The 'isAuthenticated' state variable is set based on the value in sessionStorage
+    isAuthenticated: sessionStorage.getItem("isAuthenticated") === "true",
   },
   mutations: {
     toggleAuthentication(state) {
+      // Invert the value of 'isAuthenticated' in the state
       state.isAuthenticated = !state.isAuthenticated;
+
+      // Update sessionStorage to persist the authentication state across page refreshes
+      sessionStorage.setItem("isAuthenticated", state.isAuthenticated.toString());
     },
-  },
-  actions: {
   },
   getters: {
     getIsAuthenticated: (state) => state.isAuthenticated,
