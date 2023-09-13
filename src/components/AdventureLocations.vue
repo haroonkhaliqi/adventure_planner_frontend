a25683ed732ea092ba86ccd65bd1f2b39d4b1973
elihinshaw <eghinshaw@gmail.com>
2023-09-12 19:05:32 -0500

    Added call to new /places endpoint to get place data, then properly store it in a basic card on the main route. Also deleted map from main route for now, as that will not be used for a while.

--- src/components/AdventureLocations.vue
+++ src/components/AdventureLocations.vue
@@ -4,109 +4,59 @@
     <FactoidView />
   </h3>
   <div class="locations_container">
-    <div class="add_location">
-      <form @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" class="form-control" id="name" v-model="name" />
-        </div>
-        <div class="form-group">
-          <label for="description">Description</label>
-          <textarea class="form-control" id="description" v-model="description"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="latitude">Latitude</label>
-          <input type="text" class="form-control" id="latitude" v-model="latitude" />
-        </div>
-        <div class="form-group">
-          <label for="longitude">Longitude</label>
-          <input type="text" class="form-control" id="longitude" v-model="longitude" />
-        </div>
-        <div class="form-group">
-          <button type="submit">Add Location</button>
-        </div>
-      </form>
-    </div>
-    <div class="locations_content">
-      <h1>Locations</h1>
-      <ul class="locations_list">
-        <li v-for="location in locations" :key="location.id">
-          <h2>{{ location.name }}</h2>
-          <p>{{ location.description }}</p>
-          <p>Latitude: {{ location.latitude }}</p>
-          <p>Longitude: {{ location.longitude }}</p>
-          <button @click="deleteLocation(location)">Delete</button>
+    <input class="input_container text-center" v-model="addressInput" placeholder="Enter address to search" />
+    <button @click="fetchJsonData">Search</button>
+  </div>
+  <div class="results_container">
+    <div v-if="jsonData">
+      <h4>Search Results:</h4>
+      <ul class="card-list">
+        <li v-for="(result, index) in jsonData.results" :key="index">
+          <div class="card text-dark bg-light mb-3">
+            <div class="card-header">{{ searchQuery }}</div>
+            <div class="card-body">
+              <h5 class="card-title">{{ result.name }}</h5>
+              <p class="card-text">{{ result.address }}</p>
+              <p class="card-text">{{ result.phone_number }}</p>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
   </div>
-  <AdventureMap />
 </template>
 
 <script>
-import AdventureMap from '@/components/AdventureMap.vue';
 import AdventureHeader from './AdventureHeader.vue';
 import FactoidView from './FactoidView.vue';
+import axios from "axios";
+
 export default {
   components: {
-    AdventureMap,
     AdventureHeader,
     FactoidView,
   },
   data() {
     return {
-      locations: [],
-      name: "",
-      description: "",
-      latitude: "",
-      longitude: "",
+      jsonData: null,
+      addressInput: "",
+      searchQuery: "",
     };
   },
   methods: {
-    async getData() {
-      try {
-        const response = await this.$http.get("http://localhost:8000/api/locations/");
-        this.locations = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async submitForm() {
-      try {
-        const response = await this.$http.post("http://localhost:8000/api/locations/", {
-          name: this.name,
-          description: this.description,
-          latitude: this.latitude,
-          longitude: this.longitude,
-        });
-        this.locations.push(response.data);
-        this.name = "";
-        this.description = "";
-        this.latitude = "";
-        this.longitude = "";
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async deleteLocation(location) {
-      let confirmation = confirm("Do you want to delete this location?");
+    async fetchJsonData() {
+      const address = this.addressInput;
 
-      if (confirmation) {
-        try {
-          await this.$http.delete(`http://localhost:8000/api/locations/${location.id}/`);
-          const index = this.locations.findIndex((item) => item.id === location.id);
-          if (index !== -1) {
-            this.locations.splice(index, 1);
-          }
-        } catch (error) {
-          console.log(error);
-        }
+      try {
+        const response = await axios.get(`http://localhost:8000/places?address=${address}`);
+        this.jsonData = response.data;
+        this.searchQuery = this.addressInput;
+      } catch (error) {
+        console.error("Error fetching JSON data:", error);
       }
     },
-
-  },
-  created() {
-    this.getData();
   },
 };
 </script>
+
+<style src="./AdventureLocations.css"></style>
