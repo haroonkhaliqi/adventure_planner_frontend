0810e95106a793aa27dfb62c31940ec680102992
elihinshaw <eghinshaw@gmail.com>
2023-09-27 17:06:30 -0500

    created user data component, and recreated content component

--- src/components/ContentView.vue
+++ src/components/ContentView.vue
@@ -1,31 +1,17 @@
 <template>
-  <AdventureHeader />
-  <h1 class="home-container mt-5 mb-5">HOME PAGE</h1>
-  <h4>Authentication status: <b>{{ isLoggedIn ? 'Valid' : 'Not Valid' }}</b></h4>
-  <h4>Current username: <b>{{ getUsername || "username null" }}</b></h4>
-  <h4 class="home-header mt-5" v-if="isLoggedIn">Your Token Expires In:</h4>
-  <div v-if="isLoggedIn">
-    <b>{{ getTimeDifference.hours }}</b> hours
-    <b>{{ getTimeDifference.minutes }}</b> minutes
-    <b>{{ getTimeDifference.seconds }}</b> seconds
-  </div>
-  <div v-else>
-    <b>You are not currently logged in.</b>
-  </div>
+    <AdventureHeader />
+<div>
+    <h1 class="content-header mt-3"> HOME PAGE</h1>
+</div>
 </template>
-
+  
 <script>
 import AdventureHeader from "./AdventureHeader.vue";
-import { mapGetters } from "vuex";
-
 export default {
-  components: {
-    AdventureHeader,
-  },
-  computed: {
-    ...mapGetters(["isLoggedIn", "getUsername", "getTimeDifference", "getIsExpired"]),
-  },
-};
+    components: {
+        AdventureHeader,
+    }
+}
 </script>
-
-<style src="./ContentStyling.css"></style>
+  
+<style src="./ContentViewStyling.css"></style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,23 @@
+<template>
+  <h4>Authentication status: <b>{{ isLoggedIn ? 'Valid' : 'Not Valid' }}</b></h4>
+  <h4>Current username: <b>{{ getUsername || "username null" }}</b></h4>
+  <h4 class="home-header mt-5" v-if="isLoggedIn">Your Token Expires In:</h4>
+  <div v-if="isLoggedIn">
+    <b>{{ getTimeDifference.hours }}</b> hours
+    <b>{{ getTimeDifference.minutes }}</b> minutes
+    <b>{{ getTimeDifference.seconds }}</b> seconds
+  </div>
+  <div v-else>
+    <b>You are not currently logged in.</b>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["isLoggedIn", "getUsername", "getTimeDifference", "getIsExpired"]),
+  },
+};
+</script>
