064159f8fb9d42aac4faa4ced7e441dd5a9c75ba
Haroon Khaliqi <haroonkhaliqi12@haroons-mbp.mynetworksettings.com>
2023-09-28 13:16:47 -0700

    home page

--- src/components/ContentView.vue
+++ src/components/ContentView.vue
@@ -1,17 +1,107 @@
 <template>
-    <AdventureHeader />
-<div>
-    <h1 class="content-header mt-3"> HOME PAGE</h1>
-</div>
+  <AdventureHeader />
+  <div class="bod">
+    <div class="locations_container pt-3">
+      <h3>SEARCH PLACES TO VISIT</h3>
+
+      <div class="main-search-input-item">
+        <div class="main-search-input fl-wrap">
+          <form @submit.prevent="fetchJsonData">
+            <input
+              class="input_container text-center"
+              v-model="addressInput"
+              placeholder="Where are we going?"
+              @keyup.enter="fetchJsonData"
+            />
+            <!-- <input
+        class="right_input_container text-center"
+        v-model="typeInput"
+        placeholder="Enter a type of place"
+        @keyup.enter="fetchJsonData"
+      /> -->
+          </form>
+          <button class="btn btn-dark mt-2 mb-5" @click="fetchJsonData">Search</button>
+        </div>
+      </div>
+    </div>
+    <div class="error-message pt-2" v-if="this.error">{{ this.error }}</div>
+    <!-- <div class="results_container">
+      <div v-if="jsonData">
+        <ul class="card-list">
+          <li v-for="(result, index) in jsonData.results" :key="index">
+            <div class="card text-dark bg-light mb-3">
+              <div class="card-header h3">{{ result.name }}</div>
+              <div class="card-body">
+                <details class="card-text" v-if="result.address != null">
+                  <summary>Address</summary>
+                  {{ result.address }}
+                </details>
+                <br />
+                <details class="card-text" v-if="result.phone_number != null">
+                  <summary>Phone Number</summary>
+                  {{ result.phone_number }}
+                </details>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div> -->
+  </div>
 </template>
-  
+
 <script>
 import AdventureHeader from "./AdventureHeader.vue";
+import { mapGetters } from "vuex";
+import axios from "axios";
+
 export default {
-    components: {
-        AdventureHeader,
-    }
-}
+  components: {
+    AdventureHeader,
+  },
+  data() {
+    return {
+      jsonData: null,
+      addressInput: "",
+      typeInput: "",
+      searchQuery: "",
+      error: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["isLoggedIn"]),
+    isAuthenticated() {
+      return this.isLoggedIn;
+    },
+  },
+  methods: {
+    async fetchJsonData() {
+      if (!this.isLoggedIn) {
+        this.error = "User is not logged in. Please log in to search for addresses.";
+        return;
+      }
+
+      const address = this.addressInput;
+      const type = this.typeInput;
+
+      try {
+        const token = localStorage.getItem("jwtToken");
+        const headers = {
+          Authorization: `Bearer ${token}`,
+        };
+        const response = await axios.get(`http://localhost:8000/places?address=${address}&type=${type}`, { headers });
+        this.jsonData = response.data;
+        this.searchQuery = this.addressInput;
+        this.error = null;
+      } catch (error) {
+        if (error.response && error.response.data) {
+          console.error(error);
+          this.error = error.response.data.error;
+        }
+      }
+    },
+  },
+};
 </script>
-  
+
 <style src="./ContentViewStyling.css"></style>
--- src/components/PlacesView.vue
+++ src/components/PlacesView.vue
@@ -1,102 +1,15 @@
 <template>
   <AdventureHeader />
-  <div class="locations_container pt-3">
-    <h3>SEARCH PLACES TO VISIT</h3>
-    <form @submit.prevent="fetchJsonData">
-      <input
-        class="input_container text-center"
-        v-model="addressInput"
-        placeholder="Enter address to search"
-        @keyup.enter="fetchJsonData"
-      />
-      <input
-        class="right_input_container text-center"
-        v-model="typeInput"
-        placeholder="Enter a type of place"
-        @keyup.enter="fetchJsonData"
-      />
-    </form>
-    <button class="btn btn-dark mt-2 mb-5" @click="fetchJsonData">Search</button>
-  </div>
-  <div class="error-message pt-2" v-if="this.error">{{ this.error }}</div>
-  <div class="results_container">
-    <div v-if="jsonData">
-      <ul class="card-list">
-        <li v-for="(result, index) in jsonData.results" :key="index">
-          <div class="card text-dark bg-light mb-3">
-            <div class="card-header h3">{{ result.name }}</div>
-            <div class="card-body">
-              <details class="card-text" v-if="result.address != null">
-                <summary>Address</summary>
-                {{ result.address }}
-              </details>
-              <br />
-              <details class="card-text" v-if="result.phone_number != null">
-                <summary>Phone Number</summary>
-                {{ result.phone_number }}
-              </details>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
+  <div>
+    <h1 class="content-header mt-3">HOME PAGE</h1>
   </div>
 </template>
 
 <script>
 import AdventureHeader from "./AdventureHeader.vue";
-import { mapGetters } from "vuex";
-import axios from "axios";
-
 export default {
   components: {
     AdventureHeader,
-  },
-  data() {
-    return {
-      jsonData: null,
-      addressInput: "",
-      typeInput: "",
-      searchQuery: "",
-      error: "",
-    };
-  },
-  computed: {
-    ...mapGetters(["isLoggedIn"]),
-    isAuthenticated() {
-      return this.isLoggedIn;
-    },
-  },
-  methods: {
-    async fetchJsonData() {
-      if (!this.isLoggedIn) {
-        this.error =
-          "User is not logged in. Please log in to search for addresses.";
-        return;
-      }
-
-      const address = this.addressInput;
-      const type = this.typeInput;
-
-      try {
-        const token = localStorage.getItem("jwtToken");
-        const headers = {
-          Authorization: `Bearer ${token}`,
-        };
-        const response = await axios.get(
-          `http://localhost:8000/places?address=${address}&type=${type}`,
-          { headers }
-        );
-        this.jsonData = response.data;
-        this.searchQuery = this.addressInput;
-        this.error = null
-      } catch (error) {
-        if (error.response && error.response.data) {
-          console.error(error);
-          this.error = error.response.data.error;
-        }
-      }
-    },
   },
 };
 </script>
